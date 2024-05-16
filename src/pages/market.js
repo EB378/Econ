96fc5199f1ebe7e257a1f13b785b3e445f457322
EAlexBen <107051871+EB378@users.noreply.github.com>
2024-05-16 23:30:59 +0000

    new file:   public/abby.jpeg 	new file:   public/aron.jpeg 	modified:   src/pages/market.js 	new file:   src/pages/pages.module.css

--- src/pages/market.js
+++ src/pages/market.js
@@ -1,11 +1,17 @@
 import "@/app/globals.css";
 import RootLayout from "@/app/layout";
+import styles from "./pages.module.css"
+
 
 
 export default () => (
     <>
     <RootLayout>
         <p>Target Market</p>
+        <div className={styles.columns}>
+            <img className={styles.img2} src="/aron.jpeg" alt="Aron"/>
+            <img className={styles.img2} src="/abby.jpeg" alt="Abby"/>
+        </div>
     </RootLayout>
     </>
 );
